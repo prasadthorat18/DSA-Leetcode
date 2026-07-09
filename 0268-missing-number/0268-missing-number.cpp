0dd41a0be11676b90db3a4b62c606217c2f87c8c
Prasad Thorat <prasadthorat552@gmail.com>
2026-07-09 18:09:54 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.77%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& arr) {
+        int n=arr.size();
+
+        int s1=n*(n+1)/2;
+
+        int s2=0;
+        for(int i=0; i<n; i++){
+            s2=s2+arr[i];
+        }
+        return (s1-s2);
+    }
+};
