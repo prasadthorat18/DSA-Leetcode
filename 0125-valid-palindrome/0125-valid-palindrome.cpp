39c657979f169ee77e52aaf0e9c4dd439501a2d8
Prasad Thorat <prasadthorat552@gmail.com>
2026-07-11 20:07:42 +0530

    Time: 0 ms (100%), Space: 10.2 MB (32.04%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    
+        string filtered ="";
+        for(char ch : s){
+            if(isalnum(ch)){
+                filtered += tolower(ch);
+                
+            }
+        }
+        bool ans;
+        for(int i=0; i<filtered.size()/2; i++){
+            if(filtered[i] != filtered[filtered.size()-i-1]){
+                ans=false;
+                break;
+            }
+            ans=true;
+        }
+         return ans;
+    }
+};
