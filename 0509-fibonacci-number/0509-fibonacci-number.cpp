1aadbf4a539bb27b1097b3a6bb0548f310d85067
Prasad Thorat <prasadthorat552@gmail.com>
2026-07-13 19:16:38 +0530

    Time: 10 ms (46.57%), Space: 7.6 MB (88.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
+        if(n<=1) return n;
 
-        return fib(n-1)+fib(n-2);
+        int last=fib(n-1);
+        int slast=fib(n-2);
+
+        return last + slast;
+
         
     }
 };
