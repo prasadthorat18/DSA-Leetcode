ff7afa1fe8d33ed6b1cb191f86c84c34dbc8c9d7
Prasad Thorat <prasadthorat552@gmail.com>
2026-07-27 20:38:29 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,16 +3,20 @@ public:
     void moveZeroes(vector<int>& arr) {
         int n=arr.size();
 
-        vector<int> temp(n, 0);
-        int index=0;
+        int j=-1;
         for(int i=0; i<n; i++){
-            if(arr[i]!=0){
-                temp[index]=arr[i];
-                index++;
+            if(arr[i]==0){
+                j=i;
+                break;
             }
         }
-        for(int i=0; i<n; i++){
-            arr[i]=temp[i];
+        if(j == -1) return;
+
+        for(int i=j+1; i<n; i++){
+            if(arr[i]!=0){
+                swap(arr[i], arr[j]);
+                j++;
+            }
         }
     }
 };
