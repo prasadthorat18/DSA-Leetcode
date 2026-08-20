8d2059d5808600c92a2c0023662fd4e336c7f631
Prasad Thorat <prasadthorat552@gmail.com>
2026-08-20 20:12:38 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.6%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,19 +3,16 @@ public:
     int maxSubArray(vector<int>& arr) {
         int n=arr.size();
 
-        long long sum=0;
-        long long maxi= LONG_MIN;
-
+        int sum=0;
+        int maxi=INT_MIN;
         for(int i=0; i<n; i++){
-            sum=sum + arr[i];
-
-            maxi= max(sum, maxi);
-
-            if(sum < 0){
+            sum= sum+arr[i];
+            maxi= max(maxi, sum);
+            
+            if(sum<0){
                 sum=0;
             }
         }
         return maxi;
-        
     }
 };
