13c7b037cbb446f2792411428565ced2bb70c63b
Prasad Thorat <prasadthorat552@gmail.com>
2026-08-20 19:56:03 +0530

    Time: 1647 ms (5.22%), Space: 28.2 MB (75.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int majorityElement(vector<int>& arr) {
         int n= arr.size();
-
-        int cnt=0;
-        int el;
-        for(int i=0 ;i<n; i++){
-            if(cnt == 0){
-                el=arr[i];
-                cnt=1;
+        int ans=0;
+        for(int i=0; i<n; i++){
+            int cnt=0;
+            for(int j=0; j<n; j++){
+                if(arr[i] == arr[j]) cnt++;
             }
-            else if(arr[i] == el){
-                cnt++;
-            }
-            else{
-                cnt--;
+            if(cnt > n/2){
+                ans= arr[i];
+                break;
             } 
         }
-
+        return ans;
         // int cnt2=0;
         // for(int i=0; i<n; i++){
         //     if(arr[i]==el) cnt2++;
@@ -28,7 +24,7 @@ public:
 
         // the question already state that majority element is always presrent so direct print el
         
-        return el;
+        
 
         
         
