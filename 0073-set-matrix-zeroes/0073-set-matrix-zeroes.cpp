39ac706e7e788a094de77e6575a5e572297e9d4c
Prasad Thorat <prasadthorat552@gmail.com>
2026-08-23 21:27:31 +0530

    Time: 3 ms (25.63%), Space: 21 MB (42.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,28 +2,46 @@ class Solution {
 public:
 
     void setZeroes(vector<vector<int>>& arr){
-        int n=arr.size();
-        int m=arr[0].size();
+        int n= arr.size();
+        int m= arr[0].size();
 
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
+        // int row[n] => arr[i][0]
+        // int col[m] => arr[0][j]
 
-        // first pass -> if zero is there mark row[i] and col[j] = 1 repectively
+        int col0=1;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(arr[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+                if(arr[i][j] == 0) {
+                    // mark i-th row
+                    arr[i][0]=0;
+                    
+                    // mark j-th col
+                    if(j != 0)
+                        arr[0][j] = 0;
+                    else
+                        col0= 0;
                 }
             }
         }
 
-        // pass 2 -> if any moment row and col arr is marked put entire col and row as 0
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(row[i] == 1 || col[j] == 1){
-                    arr[i][j] = 0;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(arr[i][j] != 0){
+                    if(arr[0][j]==0 || arr[i][0]==0){
+                        arr[i][j]=0;
+                    }
                 }
+            }
+        }
+
+        if(arr[0][0] == 0){
+            for(int j=0; j<m; j++){
+                arr[0][j]=0;
+            }
+        }
+        if(col0 == 0){
+            for(int i=0; i<n; i++){
+                arr[i][0] = 0;
             }
         }
     }
