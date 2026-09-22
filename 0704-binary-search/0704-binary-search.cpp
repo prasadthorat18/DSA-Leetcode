1717099769a53f9304fdddf269675f6c84d77190
Prasad Thorat <prasadthorat552@gmail.com>
2026-09-22 10:11:19 +0530

    Time: 0 ms (100%), Space: 31.4 MB (6.91%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,20 +4,19 @@ public:
         return bs( arr, 0, arr.size()-1, target);  
     }
 
-    int bs(vector<int>&arr, int low, int high, int target){
+    int bs(vector<int>& arr, int low, int high, int target){
+        if(low> high) return -1;
 
-        if(low > high) return -1;
-
-        int mid = (low + high) /2;
+        int mid = (low+high) /2;
 
         if(arr[mid] == target) return mid;
 
         else if(target > arr[mid]){
             return bs(arr, mid+1, high, target);
         }
-
-        else {
-            return bs(arr, low, mid-1, target);
-        } 
+        else{
+            return bs(arr, low, mid-1,target );
+        }
     }
+    
 };
