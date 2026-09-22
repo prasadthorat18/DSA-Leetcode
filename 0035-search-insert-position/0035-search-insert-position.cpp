41389b09ccc71b0df6c4747f5ef11bb96c4abcaf
Prasad Thorat <prasadthorat552@gmail.com>
2026-09-22 13:17:32 +0530

    Time: 0 ms (100%), Space: 13.6 MB (78.17%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,22 +4,8 @@ public:
 
         int n= arr.size();
 
-        int low=0;
-        int high = n-1;
-        int ans = n;
-
-        while(low <= high){
-            int mid = (low + high)/2;
-
-            if(arr[mid] >= target){
-                ans = mid;
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
-        }
-        return ans;
+        int lb = lower_bound(arr.begin(), arr.end(), target) - arr.begin();
         
+        return lb;
     }
 };
